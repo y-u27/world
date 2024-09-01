b31833166f269ec2c7e8eae57a6f1ef7c17066e8
Atd27 <y518a480@gmail.com>
2024-09-01 23:26:08 +0900

    トップページ作成

---
+++ src/app/world/page.tsx
@@ -0,0 +1,12 @@
+import WorldTopPage from "@/components/WorldTopPage";
+import { Box } from "@chakra-ui/react";
+
+const worldTopPage = () => {
+  return (
+    <Box>
+      <WorldTopPage />
+    </Box>
+  );
+};
+
+export default worldTopPage;
---
+++ src/components/WorldTopPage.tsx
@@ -0,0 +1,14 @@
+// 世界地図表示
+// ログアウト機能追加
+
+import { Box } from '@chakra-ui/react'
+
+const WorldTopPage = () => {
+  return (
+    <Box>
+      Hello World
+    </Box>
+  )
+}
+
+export default WorldTopPage
