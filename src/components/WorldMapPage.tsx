936e9e833d91fec0e97ef18ea33ab93439c9fbd8
Atd27 <y518a480@gmail.com>
2024-09-06 00:19:12 +0900

    トップページ作成

---
+++ src/components/WorldMapPage.tsx
@@ -0,0 +1,17 @@
+// 世界地図表示
+"use client";
+
+import { Box } from "@chakra-ui/react";
+import HowToBlock from "./HowToBlock";
+
+const WorldMapPage = () => {
+  return (
+    <>
+      <Box>
+        <HowToBlock />
+      </Box>
+    </>
+  );
+};
+
+export default WorldMapPage;
