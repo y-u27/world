e645d7aa9373336b3456d0f04aca285ac3e6d1cc
Atd27 <y518a480@gmail.com>
2024-09-06 00:17:41 +0900

    トップページの使い方コンポーネント作成

---
+++ src/components/HowToBlock.tsx
@@ -0,0 +1,44 @@
+// 「使い方」表示
+// ログアウト
+
+import {
+  Box,
+  Button,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+} from "@chakra-ui/react";
+
+const HowToBlock = () => {
+  return (
+    <>
+      <Box m="10px">
+        <Popover>
+          <PopoverTrigger>
+            <Button>使い方</Button>
+          </PopoverTrigger>
+          <PopoverContent ml="10px" bg="#E6FFFA">
+            <PopoverArrow bg="#E6FFFA" />
+            <PopoverCloseButton />
+            <PopoverHeader>世界地図を使った投稿Webアプリ</PopoverHeader>
+            <PopoverBody>
+              ①好きな国や行ったことがある国の地図をクリック
+            </PopoverBody>
+            <PopoverBody>
+              ②国が表示されるので、おすすめスポットや食べ物を投稿してみましょう
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </Box>
+      <Box m="10px" position="fixed" bottom="0">
+        <Button>ログアウト</Button>
+      </Box>
+    </>
+  );
+};
+
+export default HowToBlock;
