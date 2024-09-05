48152779cba741fbbafa2f026d699dabd570c58f
Atd27 <y518a480@gmail.com>
2024-09-06 00:16:35 +0900

    国＋投稿一覧（見た目作成中）インポート

---
+++ src/app/world/[id]/page.tsx
@@ -0,0 +1,12 @@
+import CountryPostList from "@/components/CountryPostList";
+import { Box } from "@chakra-ui/react";
+
+const CountryPostLists = () => {
+  return (
+    <Box>
+      <CountryPostList />
+    </Box>
+  );
+};
+
+export default CountryPostLists;
