9a5d932b5236decb002adbbcc43c247ce954160d
Atd27 <y518a480@gmail.com>
2024-09-06 00:15:36 +0900

    新規登録（見た目）インポート

---
+++ src/app/sign/page.tsx
@@ -0,0 +1,12 @@
+import SignUp from '@/components/SignUp'
+import { Box } from '@chakra-ui/react'
+
+const signPage = () => {
+  return (
+    <Box>
+      <SignUp/>
+    </Box>
+  )
+}
+
+export default signPage
