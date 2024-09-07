82a376f1b54032e40c61fbbdac121565e21d5c82
Atd27 <y518a480@gmail.com>
2024-09-08 01:31:00 +0900

    Google世界地図表示

--- src/components/WorldMapPage.tsx
+++ src/components/WorldMapPage.tsx
@@ -3,12 +3,49 @@
 
 import { Box } from "@chakra-ui/react";
 import HowToBlock from "./HowToBlock";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const WorldMapPage = () => {
+  // const mapContainerStyle = {
+  //   width: "100%",
+  //   height: "600px",
+  // };
+
+  // const center = {
+  //   lat: 35.6762,
+  //   lng: 139.6503,
+  // };
+
+  // const zoom = 2;
+
+  // const options = {
+  //   disableDefaultUI: true, // デフォルトのUI（ズームコントロールなど）を無効化
+  //   draggable: false, // ドラッグを無効化
+  //   zoomControl: false, // ズーム操作を無効化
+  //   scrollwheel: false, // スクロールホイールによるズームを無効化
+  //   disableDoubleClickZoom: true, // ダブルクリックによるズームを無効化
+  // };
+
+  // const googleMapsApiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY;
+
+  // if (!googleMapsApiKey) {
+  //   return <Box>Google Maps API キーが見つかりません。</Box>;
+  // }
+
   return (
     <>
       <Box>
         <HowToBlock />
+        {/* <Box>
+          <LoadScript googleMapsApiKey={googleMapsApiKey}>
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              center={center}
+              zoom={zoom}
+              options={options}
+            ></GoogleMap>
+          </LoadScript>
+        </Box> */}
       </Box>
     </>
   );
