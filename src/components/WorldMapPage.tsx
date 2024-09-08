6073840c854ae416c8061ae531655110fd339f21
Atd27 <y518a480@gmail.com>
2024-09-09 00:09:34 +0900

    世界地図表示（縦・横幅を軽微修正）

--- src/components/WorldMapPage.tsx
+++ src/components/WorldMapPage.tsx
@@ -6,47 +6,45 @@ import HowToBlock from "./HowToBlock";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const WorldMapPage = () => {
-  // const mapContainerStyle = {
-  //   width: "100%",
-  //   height: "600px",
-  // };
+  const mapContainerStyle = {
+    width: "80%",
+    height: "530px",
+  };
 
-  // const center = {
-  //   lat: 35.6762,
-  //   lng: 139.6503,
-  // };
+  // 東京の緯度軽度
+  const center = {
+    lat: 35.6762,
+    lng: 139.6503,
+  };
 
-  // const zoom = 2;
+  // 世界全体表示
+  const zoom = 2;
 
-  // const options = {
-  //   disableDefaultUI: true, // デフォルトのUI（ズームコントロールなど）を無効化
-  //   draggable: false, // ドラッグを無効化
-  //   zoomControl: false, // ズーム操作を無効化
-  //   scrollwheel: false, // スクロールホイールによるズームを無効化
-  //   disableDoubleClickZoom: true, // ダブルクリックによるズームを無効化
-  // };
+  const options = {
+    disableDefaultUI: true, // デフォルトのUI（ズームコントロールなど）を無効化
+    draggable: false, // ドラッグを無効化
+    zoomControl: false, // ズーム操作を無効化
+    scrollwheel: false, // スクロールホイールによるズームを無効化
+    disableDoubleClickZoom: true, // ダブルクリックによるズームを無効化
+  };
 
-  // const googleMapsApiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY;
+  const googleMapsApiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY;
 
-  // if (!googleMapsApiKey) {
-  //   return <Box>Google Maps API キーが見つかりません。</Box>;
-  // }
+  if (!googleMapsApiKey) {
+    return <Box>Google Maps API キーが見つかりません。</Box>;
+  }
 
   return (
     <>
-      <Box>
-        <HowToBlock />
-        {/* <Box>
-          <LoadScript googleMapsApiKey={googleMapsApiKey}>
-            <GoogleMap
-              mapContainerStyle={mapContainerStyle}
-              center={center}
-              zoom={zoom}
-              options={options}
-            ></GoogleMap>
-          </LoadScript>
-        </Box> */}
-      </Box>
+      <HowToBlock />
+      <LoadScript googleMapsApiKey={googleMapsApiKey}>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={center}
+          zoom={zoom}
+          options={options}
+        ></GoogleMap>
+      </LoadScript>
     </>
   );
 };
