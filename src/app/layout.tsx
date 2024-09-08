3ee159fa1bbd0ccbc07c434b96ca9231f1981a31
Atd27 <y518a480@gmail.com>
2024-09-09 00:08:05 +0900

    言語をjaに修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={inter.className}>
         <ChakraProvider>
           <Link href="/world">
