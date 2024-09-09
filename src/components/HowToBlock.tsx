42485613581b3d166ccb9078fd97d01d94b5b0f5
Atd27 <y518a480@gmail.com>
2024-09-10 00:00:08 +0900

    Portal導入

--- src/components/HowToBlock.tsx
+++ src/components/HowToBlock.tsx
@@ -11,32 +11,35 @@ import {
   PopoverContent,
   PopoverHeader,
   PopoverTrigger,
+  Portal,
 } from "@chakra-ui/react";
 
 const HowToBlock = () => {
   return (
     <>
-      {/* <Box m="10px"> */}
-        <Popover>
-          <PopoverTrigger>
-            <Button>使い方</Button>
-          </PopoverTrigger>
-          <PopoverContent ml="10px" bg="#E6FFFA">
-            <PopoverArrow bg="#E6FFFA" />
-            <PopoverCloseButton />
-            <PopoverHeader>世界地図を使った投稿Webアプリ</PopoverHeader>
-            <PopoverBody>
-              ①好きな国や行ったことがある国の地図をクリック
-            </PopoverBody>
-            <PopoverBody>
-              ②国が表示されるので、おすすめスポットや食べ物を投稿してみましょう
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
-      {/* </Box> */}
-      {/* <Box m="10px" position="fixed" bottom="0"> */}
-        <Button position="fixed" bottom="0">ログアウト</Button>
-      {/* </Box> */}
+      <Portal>
+        <Box>
+          <Popover>
+            <PopoverTrigger>
+              <Button position="fixed" top="10">使い方</Button>
+            </PopoverTrigger>
+            <PopoverContent ml="10px" bg="#E6FFFA">
+              <PopoverArrow bg="#E6FFFA" />
+              <PopoverCloseButton />
+              <PopoverHeader>世界地図を使った投稿Webアプリ</PopoverHeader>
+              <PopoverBody>
+                ①好きな国や行ったことがある国の地図をクリック
+              </PopoverBody>
+              <PopoverBody>
+                ②国が表示されるので、おすすめスポットや食べ物を投稿してみましょう
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+          <Button position="fixed" bottom="5">
+            ログアウト
+          </Button>
+        </Box>
+      </Portal>
     </>
   );
 };
