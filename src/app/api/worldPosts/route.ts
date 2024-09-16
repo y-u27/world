6db4d4d1b1f19804581e82c796bf8bc542b3260b
Atd27 <y518a480@gmail.com>
2024-09-16 18:58:37 +0900

    投稿データの取得・作成api

--- src/app/api/worldPosts/route.ts
+++ src/app/api/worldPosts/route.ts
@@ -4,8 +4,41 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prismaClient";
 
+// 全投稿データを取得
 export async function GET() {
-  const worldPosts = await prisma.post.findMany({
-    where: {},
+  const worldPostData = await prisma.post.findMany();
+  // 取得した投稿データを返す
+  return NextResponse.json(
+    {
+      success: true,
+      message: "投稿データ取得",
+      data: worldPostData,
+    },
+    {
+      status: 200,
+    }
+  );
+}
+
+// 投稿データの作成
+export async function POST(request: Request) {
+  const { title, content, createdAt } = await request.json();
+
+  const newWorldPostData = await prisma.post.create({
+    data: {
+      title,
+      content,
+      createdAt,
+    },
   });
+  return NextResponse.json(
+    {
+      success: true,
+      message: "投稿完了！",
+      data: newWorldPostData,
+    },
+    {
+      status: 201,
+    }
+  );
 }
