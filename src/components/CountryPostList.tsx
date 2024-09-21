5778a8ac58841df77cf0472cce704fb5557a88d9
Atd27 <y518a480@gmail.com>
2024-09-21 23:30:56 +0900

    削除するかも？

--- src/components/CountryPostList.tsx
+++ src/components/CountryPostList.tsx
@@ -1,54 +1,61 @@
-"use client";
+// "use client";
 
 // 各国表示＋投稿一覧
-import { Box } from "@chakra-ui/react";
-import PostLists from "./PostLists";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+// import { Box } from "@chakra-ui/react";
+// import PostLists from "./PostLists";
+// import { GoogleMap, LoadScript } from "@react-google-maps/api";
+// import { useState } from "react";
 
-const googleMapsApiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY;
+// Google map api
+// const googleMapsApiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY;
 
-const CountryPostList = () => {
-  const mapContainerStyle = {
-    width: "100vw",
-    height: "95vh",
-  };
+// オプションデフォルト値
+// const DEFAULT_OPTIONS = {
+  // disableDefaultUI: true, // デフォルトのUI（ズームコントロールなど）を無効化
+  // draggable: true, // ドラッグを無効化
+  // zoomControl: true, // ズーム操作を無効化
+  // scrollwheel: true, // スクロールホイールによるズームを無効化
+  // disableDoubleClickZoom: true, // ダブルクリックによるズームを無効化
+// };
+
+// const CountryPostList = () => {
+  // ↓各国の緯度経度の初期値
+  // const [mapCenter, setMapCenter] = useState({ lat: 35.6762, lng: 155.6503 });
+  // ↓それぞれの国をズームの初期値
+  // const [zoom, setZoom] = useState(4.5);
+  // ↓マップのオプション
+  // const [options, setOptions] = useState(DEFAULT_OPTIONS);
+  
+  // const mapContainerStyle = {
+  //   width: "100vw",
+  //   height: "95vh",
+  // };
 
   // オーストラリアの緯度軽度
-  const center = {
-    lat: -33.8688,
-    lng: 151.2093,
-  };
+//   const center = {
+//     lat: -33.8688,
+//     lng: 151.2093,
+//   };
 
-  // 世界全体表示
-  const zoom = 4;
+//   if (!googleMapsApiKey) {
+//     return <Box>Google Maps API キーが見つかりません。</Box>;
+//   }
 
-  const options = {
-    disableDefaultUI: true, // デフォルトのUI（ズームコントロールなど）を無効化
-    draggable: true, // ドラッグを無効化
-    zoomControl: true, // ズーム操作を無効化
-    scrollwheel: false, // スクロールホイールによるズームを無効化
-    disableDoubleClickZoom: true, // ダブルクリックによるズームを無効化
-  };
+//   return (
+//     <>
+//       <Box>
+//         <PostLists />
+//         <LoadScript googleMapsApiKey={googleMapsApiKey}>
+//           <GoogleMap
+//             mapContainerStyle={mapContainerStyle}
+//             center={center}
+//             zoom={zoom}
+//             options={options}
+//           ></GoogleMap>
+//         </LoadScript>
+//       </Box>
+//     </>
+//   );
+// };
 
-  if (!googleMapsApiKey) {
-    return <Box>Google Maps API キーが見つかりません。</Box>;
-  }
-
-  return (
-    <>
-      <Box>
-        <PostLists />
-        <LoadScript googleMapsApiKey={googleMapsApiKey}>
-          <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            center={center}
-            zoom={zoom}
-            options={options}
-          ></GoogleMap>
-        </LoadScript>
-      </Box>
-    </>
-  );
-};
-
-export default CountryPostList;
+// export default CountryPostList;
