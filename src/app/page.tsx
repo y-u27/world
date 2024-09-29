b84a6b6fa5de6a3e9a7eff10893a6090ab426abf
Atd27 <y518a480@gmail.com>
2024-09-29 23:48:47 +0900

    💄 サイト紹介&使い方ページ追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import IntroHowTo from "@/components/IntroHowTo";
 import Login from "@/components/Login";
+import { Button } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <Login />
+        <IntroHowTo />
+      
     </>
   );
 }
---
+++ src/components/IntroHowTo.tsx
@@ -0,0 +1,83 @@
+import {
+  Box,
+  Button,
+  Heading,
+  ListItem,
+  OrderedList,
+  Text,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+
+const IntroHowTo = () => {
+  return (
+    <>
+      <Box position="relative" w="80%" ml="10%" mt="60px">
+        <Box
+          bgColor="yellow.50"
+          p="6"
+          position="relative"
+          >
+          <Heading
+            position="absolute"
+            top="-20px"
+            left="20px"
+            bg="white"
+            px={4}
+            transform="rotate(-3deg)"
+            bgColor="teal.100"
+            borderRadius={10}
+            size="lg"
+            bgGradient="linear(to-r, yellow.400, pink.200)"
+            >
+            どんなサイトか？
+          </Heading>
+          <Text w="100%" ml="2%">
+            <br />
+            世界地図を使ったSNS風webサイトです
+            <br />
+            国ごとのおすすめスポットや食べ物、その国に実際に行って感動したこと、「行ってみたい！」などの投稿をすることができます
+          </Text>
+        </Box>
+      </Box>
+      <Box position="relative" w="80%" ml="10%" mt="40px">
+        <Box
+          p="6"
+          position="relative"
+          bgColor="yellow.50"
+          >
+          <Heading
+            position="absolute"
+            top="-20px"
+            left="20px"
+            bg="white"
+            px={4}
+            transform="rotate(-3deg)"
+            bgColor="teal.100"
+            borderRadius={10}
+            size="lg"
+            bgGradient="linear(to-r, yellow.400, pink.200)"
+          >
+            使い方
+          </Heading>
+          <OrderedList w="100%" ml="2%">
+            <ListItem>好きな国や行ったことがある国をクリック</ListItem>
+            <ListItem>
+              その国の地図が表示されたら、右上にある「投稿一覧」ボタンをクリック
+            </ListItem>
+            <ListItem>
+              「投稿一覧」が表示されたら、実際に投稿してみましょう！
+            </ListItem>
+          </OrderedList>
+        </Box>
+      </Box>
+      <Box m="24px">
+        <Link href="/login">
+          <Button w="50px">ログイン</Button>
+        </Link>
+      </Box>
+    </>
+  );
+};
+
+export default IntroHowTo;
