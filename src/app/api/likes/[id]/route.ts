33990713342c9e57320719312a653892cc61781b
Atd27 <y518a480@gmail.com>
2024-10-04 23:52:26 +0900

    いいね機能のapi作成中

---
+++ src/app/api/likes/[id]/route.ts
@@ -0,0 +1,27 @@
+// いいね機能のapi
+
+// import prisma from "@/app/lib/prismaClient";
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   try {y
+//     const likePostData = await prisma.likes.findMany({
+//       include: {
+//         user:true
+//       }
+//     });
+
+
+//   }
+
+//   return NextResponse.json(
+//     {
+//       success: true,
+//       message: "いいねデータ取得",
+//       data: likePostData,
+//     },
+//     {
+//       status: 200,
+//     }
+//   )
+// }
