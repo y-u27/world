adc7630ca035c7fea8ae36780ed1d280607d67c6
Atd27 <y518a480@gmail.com>
2024-10-06 01:22:57 +0900

    いいね機能api（GET、POST）作成

--- src/app/api/likes/[id]/route.ts
+++ src/app/api/likes/[id]/route.ts
@@ -1,27 +1,59 @@
 // いいね機能のapi
 
-// import prisma from "@/app/lib/prismaClient";
-// import { NextResponse } from "next/server";
+import prisma from "@/app/lib/prismaClient";
+import { NextResponse } from "next/server";
 
-// export async function GET() {
-//   try {y
-//     const likePostData = await prisma.likes.findMany({
-//       include: {
-//         user:true
-//       }
-//     });
+// いいねデータ取得
+export async function GET() {
+  try {
+    const likePostData = await prisma.likes.findMany({
+      include: {
+        user: true,
+        post: true,
+      },
+    });
+    return NextResponse.json(
+      {
+        success: true,
+        message: "いいねデータ取得",
+        data: likePostData,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "いいねデータ取得失敗",
+      },
+      {
+        status: 200,
+      }
+    );
+  }
+}
 
+// いいねデータ作成
+export async function POST(request: Request) {
+  const { userId, postId } = await request.json();
 
-//   }
-
-//   return NextResponse.json(
-//     {
-//       success: true,
-//       message: "いいねデータ取得",
-//       data: likePostData,
-//     },
-//     {
-//       status: 200,
-//     }
-//   )
-// }
+  const newLikePostData = await prisma.likes.create({
+    data: {
+      userId,
+      postId,
+    }
+  })
+  return NextResponse.json(
+    {
+      success: true,
+      message: "いいね！",
+      data: newLikePostData,
+    },
+    {
+      status: 201,
+    }
+  ) 
+}
