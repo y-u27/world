f612e7c126119e4acf0d8fe13208aab7a3c64875
Atd27 <y518a480@gmail.com>
2024-10-09 01:09:42 +0900

    国名（countryName）カラム追加

---
+++ prisma/migrations/20241007115057_delete_table_likes/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Likes` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Likes" DROP CONSTRAINT "Likes_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Likes" DROP CONSTRAINT "Likes_userId_fkey";
+
+-- DropTable
+DROP TABLE "Likes";
