4f3249818b9bc2213c488397c2bb8c2453c44b50
Atd27 <y518a480@gmail.com>
2024-10-09 01:12:03 +0900

    国名取得API作成中

---
+++ src/app/api/world-posts/[countryname]/route.ts
@@ -0,0 +1,19 @@
+// 国名取得API
+
+// import prisma from "@/app/lib/prismaClient";
+// import { NextResponse } from "next/server";
+
+// interface Params {
+//   countryname: string;
+// }
+
+// export async function GET(
+//   request: Request,
+//   { params }: { params: { countryname: Params } }
+// ) {
+//   const countryname = params.countryname;
+
+//   const countryNamePostData = await prisma.post.findMany({
+//     where: { countryName:countryname },
+//   });
+// }
