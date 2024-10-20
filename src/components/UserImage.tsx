a108c14f4ca9ec57c0ec2df5f3c2c54bd86b096a
Atd27 <y518a480@gmail.com>
2024-10-20 23:19:15 +0900

    ユーザーアイコン設定コンポーネント作成

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -1,7 +1,33 @@
+import { Avatar, Box, Button, Input } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { supabase } from "../../utils/supabase/supabase";
 
 const UserImage = () => {
-  return <div></div>;
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const { data } = await supabase.storage
+        .from("user-image-buket")
+        .getPublicUrl("IMG_2483.jpeg");
+
+      if (data) {
+        setImageUrl(data.publicUrl);
+      }
+    };
+    fetchImage();
+  }, []);
+
+  return (
+    <Box>
+      <Avatar
+        name="User Icon"
+        src={imageUrl || undefined}
+        bg="blue.300"
+        size="md"
+      />
+    </Box>
+  );
 };
 
 export default UserImage;
