64bd46a84898dfb41e548679d7980a89289e35d5
Atd27 <y518a480@gmail.com>
2024-10-22 00:20:21 +0900

    軽微修正

--- src/app/world/page.tsx
+++ src/app/world/page.tsx
@@ -1,9 +1,14 @@
 import WorldMapPage from "@/components/WorldMapPage";
 
-const worldTopPage = () => {
+type CountryProps = {
+  id: number;
+  countryName: string;
+};
+
+const worldTopPage = ({ id, countryName }: CountryProps) => {
   return (
     <>
-      <WorldMapPage />
+      <WorldMapPage id={id} countryName={countryName} />
     </>
   );
 };
