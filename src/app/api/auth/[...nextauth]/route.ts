c014985601af76c66b7339bd8db3535218492248
Atd27 <y518a480@gmail.com>
2024-10-24 23:56:24 +0900

    認証機能設定中

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    // CredentialsProvider({
+    //   credentials: {
+    //     email: { label: "Email", type: "email" },
+    //     password: { label: "Password", type: "password" },
+    //   },
+    //   async authorize(credentials) {
+    //     const res = await fetch(`${process.env.}`)
+    //   }
+    // }),
+  ],
+  secret: process.env.SECRET,
+});
+
+export { handler as GET, handler as POST };
