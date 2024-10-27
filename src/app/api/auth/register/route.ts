8aae9ebcf66b872641db5462003523e817dae19c
Atd27 <y518a480@gmail.com>
2024-10-27 19:21:19 +0900

    新規登録機能のroute.ts作成中

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest) {
+  const { email, password, image } = await request.json();
+
+  if (!email || !password) {
+    return NextResponse.json(
+      { message: "メールアドレスとパスワード登録が必要です" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        image: image || "",
+      },
+    });
+    return NextResponse.json({ user: newUser }, { status: 201 });
+  } catch (error) {
+    console.error("登録できませんでした", error);
+    return NextResponse.json({ message: "サーバーエラー" }, { status: 500 });
+  }
+}
