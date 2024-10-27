dd3265756446d149e078feebba73c7a6411240ff
Atd27 <y518a480@gmail.com>
2024-10-28 00:24:02 +0900

    修正

---
+++ prisma/migrations/20241027145524_add_userid_default/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+CREATE SEQUENCE user_userid_seq;
+ALTER TABLE "User" ALTER COLUMN "userId" SET DEFAULT nextval('user_userid_seq');
+ALTER SEQUENCE user_userid_seq OWNED BY "User"."userId";
