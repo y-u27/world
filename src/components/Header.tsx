419755021d7cb616a691414b9965d39f36e3c301
Atd27 <y518a480@gmail.com>
2024-11-04 00:14:06 +0900

    ログイン中のユーザーアイコンをヘッダーに表示

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,30 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Avatar, Box, Heading, Text } from "@chakra-ui/react";
+import { createClient } from "@supabase/supabase-js";
 import { useSession } from "next-auth/react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
 const Header = () => {
-  
+  const { data: session } = useSession();
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchAvatarUrl = async () => {
+      if (session?.user?.id) {
+        const { data } = supabase.storage
+          .from("user-image-buket")
+          .getPublicUrl(`${session?.user?.id}/icon.png`);
+
+        setAvatarUrl(data?.publicUrl ?? "/default-avatar.png");
+      }
+    };
+    fetchAvatarUrl();
+  }, [session]);
+
   return (
     <>
       <Box>
@@ -17,8 +38,16 @@ const Header = () => {
           p={2}
         >
           World Map SNS
+          {session && (
+            <Box display="flex" justifyContent="flex-end" position="absolute" top="1px" right="10px" padding="10px">
+              <Avatar
+              size="sm"
+                src={avatarUrl ?? "/default-avatar.png"}
+                name={session.user?.name ?? "ユーザー"}
+              />
+            </Box>
+          )}
         </Heading>
-        
       </Box>
     </>
   );
