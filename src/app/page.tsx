711cc5fa58375f09ecaf7e10b512fdc0b9157bab
Atd27 <y518a480@gmail.com>
2024-11-11 23:57:21 +0900

    軽微修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import IntroHowTo from "@/components/IntroHowTo";
-import Login from "@/components/Login";
-import { Button } from "@chakra-ui/react";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-        <IntroHowTo />
-      
+      <IntroHowTo />
     </>
   );
 }
