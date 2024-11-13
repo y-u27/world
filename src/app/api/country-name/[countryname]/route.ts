30690021e233705b8b6464e5a00c6a7ba6bc2257
Atd27 <y518a480@gmail.com>
2024-11-14 01:34:55 +0900

    国名を取得した際のエラーハンドリング追加

--- src/app/api/country-name/[countryname]/route.ts
+++ src/app/api/country-name/[countryname]/route.ts
@@ -12,4 +12,24 @@ export async function GET(
   const countryNameData = await prisma.post.findMany({
     where: { countryName: countryname },
   });
+
+  if (!countryNameData || countryNameData.length === 0) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "指定された国が見つかりません",
+        data: [],
+      },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(
+    {
+      success: true,
+      message: "指定された国のデータ取得成功",
+      data: countryNameData,
+    },
+    { status: 200 }
+  );
 }
