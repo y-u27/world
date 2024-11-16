cab0a81bb358d02d20e4d8a02b2c7fe87ffc2c9d
Atd27 <y518a480@gmail.com>
2024-11-17 01:22:18 +0900

    コメントとconsole.log追加

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -1,4 +1,5 @@
 // 既に存在する画像パス（imagePath）を使って Supabase から URL を取得し、そのURLをアイコンとして表示するコンポーネント
+// 目的:1つの画像パス (imagePath) を使って、Supabase ストレージから画像の公開URLを取得し、その画像を Avatar として表示
 
 import { Avatar, Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
@@ -16,6 +17,7 @@ const UserImage: React.FC<UserImageProps> = ({
   const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   useEffect(() => {
+    console.log("imagePath:",imagePath)
     const fetchImage = async () => {
       // ↓imagePathが存在しない場合、APIの呼び出しやこの行以降の処理を中断し、何も返さないようにしている
       if (!imagePath) return;
@@ -23,6 +25,7 @@ const UserImage: React.FC<UserImageProps> = ({
       const { data } = await supabase.storage
         .from("user-image-buket")
         .getPublicUrl(imagePath);
+        console.log("Fetched data:",data)
       setImageUrl(data.publicUrl);
     };
     fetchImage();
