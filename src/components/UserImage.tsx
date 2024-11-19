5e2c937c56f0305e0b4c908ac13b8a516a3a1257
Atd27 <y518a480@gmail.com>
2024-11-20 01:01:34 +0900

    supabase storage取得処理削除

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -2,8 +2,6 @@
 // 目的:1つの画像パス (imagePath) を使って、Supabase ストレージから画像の公開URLを取得し、その画像を Avatar として表示
 
 import { Avatar, Box } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { supabase } from "../../utils/supabase/supabase";
 
 type UserImageProps = {
   imagePath: string;
@@ -14,28 +12,11 @@ const UserImage: React.FC<UserImageProps> = ({
 }: {
   imagePath: string;
 }) => {
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-
-  useEffect(() => {
-    console.log("imagePath:",imagePath)
-    const fetchImage = async () => {
-      // ↓imagePathが存在しない場合、APIの呼び出しやこの行以降の処理を中断し、何も返さないようにしている
-      if (!imagePath) return;
-
-      const { data } = await supabase.storage
-        .from("user-image-buket")
-        .getPublicUrl(imagePath);
-        console.log("Fetched data:",data)
-      setImageUrl(data.publicUrl);
-    };
-    fetchImage();
-  }, [imagePath]);
-
-  if (!imageUrl) return <Box>Loading...</Box>
+  if (!imagePath) return <Box>Loading...</Box>
 
   return (
     <Box>
-      <Avatar name="User Icon" src={imageUrl} bg="blue.300" size="md" />
+      <Avatar name="User Icon" src={imagePath} bg="blue.300" size="md" />
     </Box>
   );
 };
