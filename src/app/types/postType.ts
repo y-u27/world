86535cba742a729da6f9d53cae0f60cb7de14fca
Atd27 <y518a480@gmail.com>
2024-11-20 00:58:39 +0900

    name情報追加

--- src/app/types/postType.ts
+++ src/app/types/postType.ts
@@ -1,10 +1,12 @@
-export interface postType {
+export interface PostResponse {
   id: number;
   userId: number;
-  postId: number;
-  name:string;
+  user: {
+    name: string;
+    image: string;
+  };
   title: string;
   content: string;
-  imageUrl: string;
-  imagePath: string;
+  countryName: string;
+  createdAt: string;
 }
