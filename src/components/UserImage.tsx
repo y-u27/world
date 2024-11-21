85cdf22540dd6623735f5c8a66a5de8b0b89c0c4
Atd27 <y518a480@gmail.com>
2024-11-21 22:29:49 +0900

    fetch URL変更

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -1,6 +1,3 @@
-// 既に存在する画像パス（imagePath）を使って Supabase から URL を取得し、そのURLをアイコンとして表示するコンポーネント
-// 目的:1つの画像パス (imagePath) を使って、Supabase ストレージから画像の公開URLを取得し、その画像を Avatar として表示
-
 import { Avatar, Box } from "@chakra-ui/react";
 
 type UserImageProps = {
@@ -12,7 +9,7 @@ const UserImage: React.FC<UserImageProps> = ({
 }: {
   imagePath: string;
 }) => {
-  if (!imagePath) return <Box>Loading...</Box>
+  if (!imagePath) return <Box>Loading...</Box>;
 
   return (
     <Box>
