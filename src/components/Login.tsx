f3e26b5fa5edcf3994f3a757109e08ef01a306e2
Atd27 <y518a480@gmail.com>
2024-11-29 16:58:20 +0900

    修正中

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -74,7 +74,7 @@ const Login = () => {
           <Input
             type="text"
             width="400px"
-            maxWidth="800px"
+            maxWidth="700px"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
@@ -82,13 +82,13 @@ const Login = () => {
           <Input
             type="password"
             width="400px"
-            maxWidth="800px"
+            maxWidth="700px"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           <Button
             width="400px"
-            maxWidth="800px"
+            maxWidth="700px"
             _hover={{ background: "#FAF089", color: "#319795" }}
             mt="10%"
             mx="auto"
