4730933822bc67a45f0c1ea4166bf489ed155281
Atd27 <y518a480@gmail.com>
2024-12-05 23:41:30 +0900

    💄 ユーザーの登録情報コンポーネント作成

---
+++ src/components/UserInformation.tsx
@@ -0,0 +1,14 @@
+import { Box, Card, CardBody, Text } from "@chakra-ui/react";
+import React from "react";
+
+const UserInformation = () => {
+  return (
+    <Card>
+      <CardBody>
+        <Text>登録情報</Text>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default UserInformation;
