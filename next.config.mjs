900b0688e23b3dbc4da81f5c2b8381e801d88b65
Atd27 <y518a480@gmail.com>
2024-12-12 21:32:27 +0900

    修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { fs: false, child_process: false };
     return config;
   },
 };
