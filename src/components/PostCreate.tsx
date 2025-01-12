a887a466ea974df3b6a5614e460c084dad38b6f4
Atd27 <y518a480@gmail.com>
2025-01-12 22:05:33 +0900

    軽微修正

--- src/components/PostCreate.tsx
+++ src/components/PostCreate.tsx
@@ -15,8 +15,9 @@ import {
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TiArrowBackOutline } from "react-icons/ti";
+// import { useCountryContext } from "./CountryContext";
 
 const createPost = async (
   countryName: string | undefined,
@@ -45,6 +46,15 @@ const PostCreate = () => {
   const router = useRouter();
   const { data: session } = useSession();
   const searchParams = useSearchParams();
+  const [selectedCountry, setSelectedCountry] = useState<string | null>();
+
+  useEffect(() => {
+    const country = searchParams.get("country");
+
+    if (country) {
+      setSelectedCountry(country);
+    }
+  }, [searchParams, setSelectedCountry]);
 
   const handleMapPost = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -98,6 +108,7 @@ const PostCreate = () => {
                   type="text"
                   width="130%"
                   maxWidth="500px"
+                  value={selectedCountry || ""}
                   readOnly
                 />
                 <Input
