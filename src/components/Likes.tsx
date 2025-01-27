e9bc8de7d26052c05eff529708aacdcb88d22ac9
Atd27 <y518a480@gmail.com>
2025-01-27 23:59:40 +0900

    いいねコンポーネント作成

---
+++ src/components/Likes.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import { GrLike } from "react-icons/gr";
+
+const Likes = () => {
+  return (
+    <Box>
+      <GrLike />
+    </Box>
+  );
+};
+
+export default Likes;
