b8158b8f43816941c65d6f4e25f33e9d78ece173
Atd27 <y518a480@gmail.com>
2025-01-28 00:10:21 +0900

    いいねAPI追加中

--- src/components/Likes.tsx
+++ src/components/Likes.tsx
@@ -1,6 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import { GrLike } from "react-icons/gr";
 
+// const createLikes = async (userId:string,postId:string) => {
+//   const res = await fetch(`https://world-map-sns.vercel.app/api/Likes/`)
+// }
+
 const Likes = () => {
   return (
     <Box>
