c0b2ceb73c08a843974a1a6e0efdaf5835ab7a69
Atd27 <y518a480@gmail.com>
2025-02-05 18:45:18 +0900

    ローディングアイコン秒数修正

--- src/components/WorldMapPage.tsx
+++ src/components/WorldMapPage.tsx
@@ -134,7 +134,7 @@ const WorldMapPage = ({ id, userId, postId }: CountryProps) => {
           my="20%"
           mx="50%"
           thickness="4px"
-          speed="1s"
+          speed="0.8s"
           emptyColor="gray.200"
           color="teal.200"
           size="lg"
