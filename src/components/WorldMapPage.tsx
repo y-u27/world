29089b6f3aaeb9593b99fc1de0b4e4f3f5c75777
Atd27 <y518a480@gmail.com>
2025-02-05 12:14:05 +0900

    ローディングアイコン位置修正

--- src/components/WorldMapPage.tsx
+++ src/components/WorldMapPage.tsx
@@ -131,11 +131,12 @@ const WorldMapPage = ({ id, userId, postId }: CountryProps) => {
     return (
       <Box>
         <Spinner
+          m="0"
           thickness="4px"
           speed="0.65s"
           emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
+          color="teal.200"
+          size="md"
         />
       </Box>
     );
