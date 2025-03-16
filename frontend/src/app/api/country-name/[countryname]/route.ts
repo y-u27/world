1bf9c10e45779fe5b8efc46c3c0f4314b4c20f0f
Atd27 <y518a480@gmail.com>
2025-03-16 16:56:21 +0900

    修正

--- frontend/src/app/api/country-name/[countryname]/route.ts
+++ frontend/src/app/api/country-name/[countryname]/route.ts
@@ -2,11 +2,19 @@
 import prisma from "../../../lib/prismaClient";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { countryname: string } }
-) {
-  const { countryname } = params;
+interface Context {
+  params: { countryname: string };
+}
+
+export async function GET(request: NextRequest, context: Context) {
+  const { countryname } = context.params;
+
+  if (!countryname) {
+    return NextResponse.json(
+      { success: false, message: "国名が指定されていません" },
+      { status: 400 }
+    );
+  }
 
   const countryNameData = await prisma.post.findMany({
     where: { countryName: countryname },
