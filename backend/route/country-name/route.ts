226b6ad40d93ad208a5bf89e3a663a4a3618a483
Atd27 <y518a480@gmail.com>
2025-03-20 01:47:28 +0900

    国名取得API

--- backend/route/country-name/route.ts
+++ backend/route/country-name/route.ts
@@ -1,8 +1,42 @@
 // expressに書き換え
 import prisma from "../../../frontend/src/app/lib/prismaClient";
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 const router = Router();
 
 // 国名取得API
-// app.get("/")
+router.get("/post/:id", async (req: Request, res: Response): Promise<void> => {
+  const params = req.params;
+  const countryname = params.countryname;
+
+  // countrynameが空欄の場合
+  if (!countryname) {
+    res
+      .status(400)
+      .json({ success: false, message: "国名が指定されていません" });
+    return;
+  }
+
+  // 各国名の投稿データ取得
+  const countryNameData = await prisma.post.findMany({
+    where: { countryName: countryname },
+  });
+
+  // countryNameDataではなく、またcountryNameDataの長さが0の場合
+  if (!countryNameData || countryNameData.length === 0) {
+    res.status(404).json({
+      success: false,
+      message: "指定された国が見つかりません",
+      data: [],
+    });
+    return;
+  }
+
+  // 指定された国名のデータが見つかった場合
+  res.status(200).json({
+    success: true,
+    message: "指定された国のデータが見つかりました",
+    data: countryNameData,
+  });
+  return;
+});
