bcab353f43a0695235ef4a2b5568d6b42975b5fb
Atd27 <y518a480@gmail.com>
2025-03-20 23:00:12 +0900

    パス修正

---
+++ backend/route/user/index.ts
@@ -0,0 +1,9 @@
+import prisma from "../../../frontend/src/app/lib/prismaClient";
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+// ユーザー情報を取得するAPI
+router.get("/user",async(req:Request,res:Response):Promise<void> => {
+  // let session = Session
+})
