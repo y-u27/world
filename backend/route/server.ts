48a7c1ae735c16b715e0b834fec0100b8e98e9e0
Atd27 <y518a480@gmail.com>
2025-03-22 01:14:09 +0900

    express-session導入

--- backend/route/server.ts
+++ backend/route/server.ts
@@ -1,10 +1,14 @@
 import worldPostApiRoute from "./world-posts/index";
 
 const express = require("express");
+const session = require("express-session");
 
 const app = express();
 
 app.use(express.json());
+
+// セッション設定
+app.use(session({}));
 
 // ルート設定
 app.use("/api", worldPostApiRoute);
