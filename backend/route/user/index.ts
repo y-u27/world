70a25dc1e78b9aa5d08352151caabe392f89ce38
Atd27 <y518a480@gmail.com>
2025-03-22 01:14:41 +0900

    user情報取得API作成中

--- backend/route/user/index.ts
+++ backend/route/user/index.ts
@@ -1,9 +1,7 @@
 import prisma from "../../../frontend/src/app/lib/prismaClient";
 import { Request, Response, Router } from "express";
 
-const router = Router();
+const router = Router()
 
 // ユーザー情報を取得するAPI
-router.get("/user",async(req:Request,res:Response):Promise<void> => {
-  // let session = Session
-})
+router.get("/user", async (req: Request, res: Response): Promise<void> => {});
