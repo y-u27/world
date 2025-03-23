e2bc36610f5c4a6a3effd19ef04c882015fd1267
Atd27 <y518a480@gmail.com>
2025-03-24 08:17:58 +0900

    各APIルート設定

--- backend/src/route/server.ts
+++ backend/src/route/server.ts
@@ -1,5 +1,8 @@
 import worldPostApi from "./world-posts/index";
 import worldPostListApi from "./world-posts/[id]/index";
+import userApi from "./user/index";
+import likeApi from "./likes/index";
+import countryNameApi from "./country-name/index";
 
 const express = require("express");
 
@@ -10,6 +13,9 @@ app.use(express.json());
 // ルート設定
 app.use("/api", worldPostApi);
 app.use("/api", worldPostListApi);
+app.use("/api",userApi);
+app.use("/api",likeApi);
+app.use("/api",countryNameApi);
 
 // サーバ起動
 app.listen(5000);
