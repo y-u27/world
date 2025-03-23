da8571e3673e8bb1160fd3cf55a1919c7d523cba
Atd27 <y518a480@gmail.com>
2025-03-23 22:44:11 +0900

    投稿一覧表示

--- backend/src/route/server.ts
+++ backend/src/route/server.ts
@@ -1,4 +1,5 @@
-import worldPostApiRoute from "./world-posts/index";
+import worldPostApi from "./world-posts/index";
+import worldPostListApi from "./world-posts/[id]/index";
 
 const express = require("express");
 
@@ -7,7 +8,8 @@ const app = express();
 app.use(express.json());
 
 // ルート設定
-app.use("/api", worldPostApiRoute);
+app.use("/api", worldPostApi);
+app.use("/api", worldPostListApi);
 
 // サーバ起動
 app.listen(5001);
