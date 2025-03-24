403db6e3bf2430dc43fa876e892aba4055527979
Atd27 <y518a480@gmail.com>
2025-03-24 21:29:48 +0900

    コメント更新API修正

--- backend/src/route/user/index.ts
+++ backend/src/route/user/index.ts
@@ -16,24 +16,30 @@ router.post("/user", async (req: Request, res: Response): Promise<void> => {
 });
 
 // コメントを更新するAPI
-// router.patch("/user", async (req: Request, res: Response): Promise<void> => {
-//   const { email, comment } = req.query;
+router.patch("/user", async (req: Request, res: Response): Promise<void> => {
+  const email =
+    typeof req.query.email === "string" ? req.query.email : undefined;
+  const comment =
+    typeof req.query.comment === "string" ? req.query.comment : undefined;
 
-//   if (!comment) {
-//     res.status(400).json({ error: "コメントが更新できませんでした" });
-//     return;
-//   }
+  if (!comment) {
+    res.status(400).json({ error: "コメントが更新できませんでした" });
+    return;
+  }
 
-//   const updateComment = await prisma.user.update({
-//     where: { email } as string[],
-//     data: { comment },
-//   });
-//   res.status(200).json({
-//     success: true,
-//     message: "コメントを更新できました",
-//     data: updateComment,
-//   });
-//   return;
-// });
+  try {
+    const updateComment = await prisma.user.update({
+      where: { email },
+      data: { comment },
+    });
+    res.status(200).json({
+      success: true,
+      message: "コメントを更新できました",
+      data: updateComment,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "コメント更新処理に失敗しました" });
+  }
+});
 
 export default router;
