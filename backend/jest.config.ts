b314456942658c6906f19d718ff6cc6249584cb3
Atd27 <y518a480@gmail.com>
2025-03-31 17:15:11 +0900

    修正

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,9 +3,6 @@ import type { JestConfigWithTsJest } from "ts-jest";
 const config: JestConfigWithTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["/backend/tests/server.test.ts"],
-  moduleFileExtensions: ["ts", "js", "json", "node"],
-  transform: { "^.+\\.ts$": "ts-jest" },
 };
 
 export default config;
