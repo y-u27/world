77abf24bf4e0db5ba93b3678ef32b3c3d8382299
Atd27 <y518a480@gmail.com>
2025-04-03 14:47:00 +0900

    registerAPI作成

---
+++ backend/src/route/register/index.ts
@@ -0,0 +1,34 @@
+import prisma from "../../../lib/prismaClient";
+import { Request, Response, Router } from "express";
+import bycrypt from "bcrypt";
+
+const router = Router();
+
+router.post("/register", async (req: Request, res: Response): Promise<void> => {
+  const { name, email, password, image } = req.body;
+
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ message: "メールアドレスとパスワード登録が必要です" });
+    return;
+  }
+
+  try {
+    const hashePassword = await bycrypt.hash(password, 0);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashePassword,
+        image: image || "",
+      },
+    });
+    res.status(200).json({ user: newUser });
+    return;
+  } catch (error) {
+    res.status(500).json({ message: "登録できませんでした,error" });
+    return;
+  }
+});
