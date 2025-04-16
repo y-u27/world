cf4928f049cbaaa1d4b15bea3f55ece4fa587554
Atd27 <y518a480@gmail.com>
2025-04-17 00:48:03 +0900

    POSTメソッド追記

--- frontend/src/components/UserInformation.tsx
+++ frontend/src/components/UserInformation.tsx
@@ -36,7 +36,16 @@ const UserInformation: React.FC<UserInformationProps> = ({
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user`);
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/user`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email }),
+          }
+        );
         const data = await res.json();
         if (res.ok) {
           setComment(data.data.comment || "");
@@ -49,7 +58,7 @@ const UserInformation: React.FC<UserInformationProps> = ({
       }
     };
     fetchUserData();
-  }, [comment]);
+  }, []);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     //入力中の値を更新
