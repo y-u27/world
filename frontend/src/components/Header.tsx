b4a699c28df7fd8d921958477763bac4417b7ef9
y-u27 <161412624+y-u27@users.noreply.github.com>
2025-06-23 08:47:45 +0900

    Header.tsx を更新

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -86,7 +86,7 @@ const Header = () => {
                   name={session.user?.name ?? "ユーザー"}
                 />
               </Link>
-              <Box position="absolute" right="1500px">
+              <Box position="absolute" right="1100px">
                 <BiLogOut onClick={handleLogout} />
               </Box>
             </Box>
