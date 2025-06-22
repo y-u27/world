46f82006d0720537a989624503c018067033d738
Atd27 <y518a480@gmail.com>
2025-06-22 22:21:40 +0900

    ログアウトアイコン位置修正

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -86,7 +86,7 @@ const Header = () => {
                   name={session.user?.name ?? "ユーザー"}
                 />
               </Link>
-              <Box position="absolute" right="300%">
+              <Box position="absolute" right="600%">
                 <BiLogOut onClick={handleLogout} />
               </Box>
             </Box>
