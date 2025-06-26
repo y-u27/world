851a06716afaf7aae91a585e9d7f82e1c01c8d8d
Atd27 <y518a480@gmail.com>
2025-06-27 00:03:18 +0900

    ✨ 検索窓作成

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Box, Collapse, IconButton, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import { TbMapSearch } from "react-icons/tb";
+
+const SearchBar = () => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleSearchIcon = () => {
+    setIsSearchOpen((prev) => !prev);
+  };
+
+  return (
+    <Box position="relative" display="flex" alignItems="center">
+      {/* 検索アイコン */}
+      <IconButton
+        aria-label="検索"
+        icon={<TbMapSearch />}
+        onClick={toggleSearchIcon}
+        size="sm"
+        variant="ghost"
+        mr={2}
+      />
+
+      {/* 検索窓（開閉） */}
+      <Collapse in={isSearchOpen} animateOpacity>
+        <Input
+          placeholder="国名を入力"
+          background="white"
+          size="sm"
+          width="180px"
+          mr="2"
+        />
+      </Collapse>
+    </Box>
+  );
+};
+
+export default SearchBar;
