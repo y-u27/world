f791f56323caf82488671d67ff9d7a6b17d41055
Atd27 <y518a480@gmail.com>
2025-08-08 23:29:26 +0900

    🔥 不要なインポート削除

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,7 +12,6 @@ import {
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-// import { TbMapSearch } from "react-icons/tb";
 import { BiLogOut } from "react-icons/bi";
 import { useRouter } from "next/navigation";
 import SearchBar from "./SearchBar";
