88ea4944d92186559e87915f09bc49143dd86d9c
Atd27 <y518a480@gmail.com>
2025-08-18 01:13:55 +0900

    エラー時のコンソール追加

--- backend/src/route/register/index.ts
+++ backend/src/route/register/index.ts
@@ -4,36 +4,34 @@ import bcrypt from "bcrypt";
 
 const router = Router();
 
-router.post(
-  "/register",
-  async (req: Request, res: Response): Promise<void> => {
-    const { name, email, password, image } = req.body;
+router.post("/register", async (req: Request, res: Response): Promise<void> => {
+  const { name, email, password, image } = req.body;
 
-    if (!email || !password) {
-      res
-        .status(400)
-        .json({ message: "メールアドレスとパスワード登録が必要です" });
-      return;
-    }
-
-    try {
-      const hashePassword = await bcrypt.hash(password, 10);
-
-      const newUser = await prisma.user.create({
-        data: {
-          name,
-          email,
-          password: hashePassword,
-          image: image || "",
-        },
-      });
-      res.status(200).json({ user: newUser });
-      return;
-    } catch (error) {
-      res.status(500).json({ message: "登録できませんでした,error" });
-      return;
-    }
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ message: "メールアドレスとパスワード登録が必要です" });
+    return;
   }
-);
+
+  try {
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+        image: image || "",
+      },
+    });
+    res.status(200).json({ user: newUser });
+    return;
+  } catch (error) {
+    console.error("Register error: ", error);
+    res.status(500).json({ message: "登録できませんでした,error" });
+    return;
+  }
+});
 
 export default router;
