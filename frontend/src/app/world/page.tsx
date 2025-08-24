18f4936492d8e74fd04b2c06560e673dc58fd51e
Atd27 <y518a480@gmail.com>
2025-08-24 20:54:08 +0900

    親コンポーネントにprops設定

--- frontend/src/app/world/page.tsx
+++ frontend/src/app/world/page.tsx
@@ -6,7 +6,12 @@ import { redirect } from "next/navigation";
 const WorldTopPage = async () => {
   const session = await getServerSession(authOptions);
   if (!session) return redirect("/login");
-  return <WorldMapPage userId={session.user.id} />;
+  return (
+    <WorldMapPage
+      userId={session.user.id}
+      imagePostPath={session.user.imagePostPath}
+    />
+  );
 };
 
 export default WorldTopPage;
