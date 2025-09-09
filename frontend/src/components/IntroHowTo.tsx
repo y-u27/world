f2cdd618bad2a39de7bb4088d575c9bd5e97a705
Atd27 <y518a480@gmail.com>
2025-09-09 23:56:29 +0900

    UI修正

--- frontend/src/components/IntroHowTo.tsx
+++ frontend/src/components/IntroHowTo.tsx
@@ -92,8 +92,8 @@ const IntroHowTo = () => {
                         <Image
                           src="/sample/スクリーンショット 2024-09-30 20.28.38.jpeg"
                           alt="Top Image"
-                          width={300}
-                          height={600}
+                          // width="300px"
+                          // height="600px"
                           style={{ width: "100%", height: "auto" }}
                         />
                       </Box>
