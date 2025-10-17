964956d04df9eb655bfe4a6e474c03c78f84dc9e
Atd27 <y518a480@gmail.com>
2025-10-17 23:20:12 +0900

    userIdApi追加

--- backend/dist/src/route/server.js
+++ backend/dist/src/route/server.js
@@ -8,9 +8,10 @@ const cors_1 = __importDefault(require("cors"));
 const index_1 = __importDefault(require("./world-posts/index"));
 const index_2 = __importDefault(require("./world-posts/[id]/index"));
 const index_3 = __importDefault(require("./user/index"));
-const index_4 = __importDefault(require("./likes/index"));
-const index_5 = __importDefault(require("./country-name/index"));
-const index_6 = __importDefault(require("./register/index"));
+const index_4 = __importDefault(require("./world-posts/userId/index"));
+const index_5 = __importDefault(require("./likes/index"));
+const index_6 = __importDefault(require("./country-name/index"));
+const index_7 = __importDefault(require("./register/index"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 console.log("サーバー起動中...");
@@ -27,9 +28,10 @@ app.use((0, cors_1.default)({
 app.use("/api", index_1.default);
 app.use("/api", index_2.default);
 app.use("/api", index_3.default);
-app.use("/api", index_4.default);
 app.use("/api", index_5.default);
 app.use("/api", index_6.default);
+app.use("/api", index_7.default);
+app.use("/api", index_4.default);
 // サーバ起動
 app.listen(port, () => {
     console.log(`サーバーが${process.env.NEXT_PUBLIC_BASE_URL}で起動`);
