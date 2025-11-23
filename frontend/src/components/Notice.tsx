3edb5cbe6c29fb41b3b056c127f80d652b21dd00
Atd27 <y518a480@gmail.com>
2025-11-23 16:45:14 +0900

    お知らせコンポーネント作成

---
+++ frontend/src/components/Notice.tsx
@@ -0,0 +1,40 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Notice = () => {
+  return (
+    <>
+      <Box>
+        <Box
+          bgColor="yellow.50"
+          p={[4, 6, 8]}
+          h="100%"
+          position="relative"
+          boxShadow="xl"
+          borderRadius="10px"
+        >
+          <Heading
+            position="absolute"
+            top="-20px"
+            left="20px"
+            bg="white"
+            px={[2, 4, 6]}
+            transform="rotate(-3deg)"
+            bgColor="teal.100"
+            borderRadius={10}
+            bgGradient="linear(to-r, teal.300, blue.200,gray.100)"
+            fontWeight="none"
+            size={["md", "lg", "xl"]}
+          >
+            お知らせ
+          </Heading>
+          <Text w="100%" ml="2%">
+            {/* お知らせAPIを介してNewsテーブルからデータ取得 */}
+          </Text>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Notice;
