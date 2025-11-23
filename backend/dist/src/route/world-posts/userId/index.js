114c2e28d5c1d94b96558283869f3d1c050ea8ff
Atd27 <y518a480@gmail.com>
2025-11-23 16:29:11 +0900

    backendのworld-posts＞userId APIのコンパイル

--- backend/dist/src/route/world-posts/userId/index.js
+++ backend/dist/src/route/world-posts/userId/index.js
@@ -1,4 +1,6 @@
 "use strict";
+//ユーザーページの「投稿記事」の編集・削除機能追加
+//patch、deleteメソッドにて処理を追加する
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
