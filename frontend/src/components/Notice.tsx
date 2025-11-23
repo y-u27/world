59f4a6306c3363c8f80e5661c39b2ede7167d6c3
Atd27 <y518a480@gmail.com>
2025-11-23 22:23:56 +0900

    UI修正

--- frontend/src/components/Notice.tsx
+++ frontend/src/components/Notice.tsx
@@ -4,32 +4,34 @@ import React from "react";
 const Notice = () => {
   return (
     <>
-      <Box
-        bgColor="yellow.50"
-        p={[4, 6, 8]}
-        h="100%"
-        position="relative"
-        boxShadow="xl"
-        borderRadius="10px"
-      >
-        <Heading
-          position="absolute"
-          top="-20px"
-          left="20px"
-          bg="white"
-          px={[2, 4, 6]}
-          transform="rotate(-3deg)"
-          bgColor="teal.100"
-          borderRadius={10}
-          bgGradient="linear(to-r, teal.300, blue.200,gray.100)"
-          fontWeight="none"
-          size={["md", "lg", "xl"]}
+      <Box w="100%">
+        <Box
+          bgColor="yellow.50"
+          p={[4, 6, 8]}
+          h="100%"
+          position="relative"
+          boxShadow="xl"
+          borderRadius="10px"
         >
-          お知らせ
-        </Heading>
-        <Text w="100%" ml="2%">
-          {/* お知らせAPIを介してNewsテーブルからデータ取得 */}
-        </Text>
+          <Heading
+            position="absolute"
+            top="-20px"
+            left="20px"
+            bg="white"
+            px={[2, 4, 6]}
+            transform="rotate(-3deg)"
+            bgColor="teal.100"
+            borderRadius={10}
+            bgGradient="linear(to-r, teal.300, blue.200,gray.100)"
+            fontWeight="none"
+            size={["md", "lg", "xl"]}
+          >
+            お知らせ
+          </Heading>
+          <Text w="100%" ml="2%">
+            {/* お知らせAPIを介してNewsテーブルからデータ取得 */}
+          </Text>
+        </Box>
       </Box>
     </>
   );
