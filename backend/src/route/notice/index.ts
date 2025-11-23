8a449cf24af3d1acf5ccce3a9725c5edc744e20b
Atd27 <y518a480@gmail.com>
2025-11-23 23:09:01 +0900

    全お知らせデータ取得APIの修正（最新の日時順へ変更）

--- backend/src/route/notice/index.ts
+++ backend/src/route/notice/index.ts
@@ -6,17 +6,15 @@ const router = Router();
 //全お知らせデータ取得
 router.get("/notice", async (req: Request, res: Response): Promise<void> => {
   try {
-    const id = parseInt(req.params.id);
-
-    const noticeDataId = await prisma.news.findUnique({
-      where: { id },
+    const noticeDataAll = await prisma.news.findMany({
+      orderBy: { createdAt: "desc" },
     });
 
     //お知らせデータ取得成功
     res.status(200).json({
       success: true,
       message: "お知らせデータ取得成功",
-      data: noticeDataId,
+      data: noticeDataAll,
     });
   } catch (err) {
     console.error("お知らせデータ取得失敗", err);
