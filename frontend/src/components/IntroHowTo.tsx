bec653ba9b74514763993d8f3c6923b1f441dd8b
Atd27 <y518a480@gmail.com>
2025-12-14 01:34:53 +0900

    UI修正

--- frontend/src/components/IntroHowTo.tsx
+++ frontend/src/components/IntroHowTo.tsx
@@ -143,7 +143,7 @@ const IntroHowTo = () => {
         </Box>
         <Box
           w={["90%", "80%", "70%"]}
-          mb="5%"
+          mt="5%"
           display="flex"
           justifyContent="center"
         >
