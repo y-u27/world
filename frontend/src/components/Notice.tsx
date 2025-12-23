73ea783ecdefbaaced9e7704b7c740c7154f64f5
Atd27 <y518a480@gmail.com>
2025-12-24 00:04:26 +0900

    微調整

--- frontend/src/components/Notice.tsx
+++ frontend/src/components/Notice.tsx
@@ -63,7 +63,7 @@ const Notice = () => {
             お知らせ
           </Heading>
           {/* お知らせAPIを介してNewsテーブルからデータ取得 */}
-          <Box height="5em" overflow="auto">
+          <Box height="3em" overflow="auto">
             {notices.length === 0 ? (
               <Text mt="30px" ml="2%" color="gray.500">
                 ※お知らせはありません
