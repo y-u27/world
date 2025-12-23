e5ff287290862e7885483c3e5913c0d3a08a0db0
Atd27 <y518a480@gmail.com>
2025-12-23 23:51:19 +0900

    UI修正

--- frontend/src/components/Notice.tsx
+++ frontend/src/components/Notice.tsx
@@ -63,7 +63,7 @@ const Notice = () => {
             お知らせ
           </Heading>
           {/* お知らせAPIを介してNewsテーブルからデータ取得 */}
-          <Box>
+          <Box height="5px" overflow="auto">
             {notices.length === 0 ? (
               <Text mt="30px" ml="2%" color="gray.500">
                 ※お知らせはありません
