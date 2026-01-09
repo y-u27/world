e4ef287fec4246a5cb6885fe13fa845be20a66ba
Atd27 <y518a480@gmail.com>
2026-01-09 23:20:10 +0900

    userName,commentの非表示

--- frontend/src/components/UserImage2.tsx
+++ frontend/src/components/UserImage2.tsx
@@ -2,31 +2,21 @@ import { Avatar, Box, Text } from "@chakra-ui/react";
 
 type UserImagesProp = {
   imagePath: string;
-  userName: string;
-  comment: string;
 };
 
 const UserImage2: React.FC<UserImagesProp> = ({
   imagePath,
-  userName,
-  comment,
 }: {
   imagePath: string;
-  userName: string;
-  comment: string;
 }) => {
   return (
     <Box>
       <Avatar
-        name={userName || "User Icon"}
+        name="User Icon"
         src={imagePath || "/default-avatar.jpeg"}
         bg="blue.300"
         size="md"
       />
-      <Text fontSize="lg" fontWeight="bold">
-        {userName}
-      </Text>
-      <Text textAlign="center">{comment}</Text>
     </Box>
   );
 };
