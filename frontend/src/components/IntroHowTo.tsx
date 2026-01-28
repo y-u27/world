f61d2bfce99fd4cbb5287f1b0db0a9a16b6da8f2
Atd27 <y518a480@gmail.com>
2026-01-28 23:03:22 +0900

    位置微調整

--- frontend/src/components/IntroHowTo.tsx
+++ frontend/src/components/IntroHowTo.tsx
@@ -149,7 +149,7 @@ const IntroHowTo = () => {
         >
           <Notice />
         </Box>
-        <Box mt="30px" textAlign="center">
+        <Box mt="20px" textAlign="center">
           <Link href="/login">
             <Button
               w={["90%", "90%", "300px"]}
