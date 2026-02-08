95435bde4d75bab7d8ade384548d221071d03336
Atd27 <y518a480@gmail.com>
2026-02-09 00:49:42 +0900

    next用のjestコードへ書き換え

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
-  },
 };
+
+module.exports = createJestConfig(customJestConfig);
