b10993fb25c321ba1d47392ca6355f2a199fb5a0
Atd27 <y518a480@gmail.com>
2026-02-09 00:49:57 +0900

    テストコード実装中

--- frontend/src/components/Header.test.tsx
+++ frontend/src/components/Header.test.tsx
@@ -1,9 +1,14 @@
-import { render, screen } from "@testing-library/react"
-import Header from "./Header"
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+import { SessionProvider } from "next-auth/react";
 
 describe("Header", () => {
   it("タイトル表示", () => {
-    render(<Header/>);
-    expect(screen.getByText("My App")).toBeInTheDocument();
-  })
-})
+    render(
+      <SessionProvider session={null}>
+        <Header />
+      </SessionProvider>
+    );
+    expect(screen.getByText("World Map SNS")).toBeInTheDocument();
+  });
+});
