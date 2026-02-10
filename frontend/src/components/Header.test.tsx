e5280bc7c3d4b5ce66be2a02ebf9ff3f59b72808
Atd27 <y518a480@gmail.com>
2026-02-11 00:02:40 +0900

    テストコード追記

--- frontend/src/components/Header.test.tsx
+++ frontend/src/components/Header.test.tsx
@@ -1,13 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 import { SessionProvider } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+jest.mock("next/navigation", () => {
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    refresh: jest.fn(),
+    back: jest.fn(),
+  });
+});
 
 describe("Header", () => {
   it("タイトル表示", () => {
     render(
       <SessionProvider session={null}>
         <Header />
-      </SessionProvider>
+      </SessionProvider>,
     );
     expect(screen.getByText("World Map SNS")).toBeInTheDocument();
   });
