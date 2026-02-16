1e0f0c1b0cfd947ed156377500b6cda892af1b18
Atd27 <y518a480@gmail.com>
2026-02-16 23:52:41 +0900

    ✅非同期処理へのテストコード記述

--- frontend/src/components/IntroHowTo.test.tsx
+++ frontend/src/components/IntroHowTo.test.tsx
@@ -2,13 +2,19 @@ import { render, screen } from "@testing-library/react";
 import IntroHowTo from "./IntroHowTo";
 import { ChakraProvider } from "@chakra-ui/react";
 
+global.fetch = jest.fn(() => 
+  Promise.resolve({
+    json: () => Promise.resolve([]),
+  })
+) as jest.Mock
+
 describe("IntroHowTo", () => {
-  it("「使い方」表示", () => {
+  it("「使い方」表示", async () => {
     render(
       <ChakraProvider>
         <IntroHowTo />
       </ChakraProvider>,
     );
-    expect(screen.getByText("使い方")).toBeInTheDocument();
+    expect(await screen.findByText("使い方")).toBeInTheDocument();
   });
 });
