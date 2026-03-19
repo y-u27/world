8d4e75a7b379dec6ba623bbca1b91833c18e935f
Atd27 <y518a480@gmail.com>
2026-03-20 00:12:07 +0900

    UI修正

--- frontend/src/components/UserInformation.tsx
+++ frontend/src/components/UserInformation.tsx
@@ -287,7 +287,7 @@ const UserInformation: React.FC<UserInformationProps> = ({
                 >
                   投稿記事
                 </Text>
-                <Box maxH="200px" overflow="auto">
+                <Box maxH="280px" overflow="auto">
                   {!userPosts || userPosts.length === 0 ? (
                     <Text>投稿はまだありません</Text>
                   ) : (
