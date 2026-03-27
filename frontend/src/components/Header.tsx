5e88399e4a06627aaf91595c06ac68c42ad02062
Atd27 <y518a480@gmail.com>
2026-03-27 11:40:49 +0900

    ヘッダーの高さ調整

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
           color="#000080"
           bgColor="#ffffff"
           textAlign="center"
-          h="80%"
+          h="90%"
           size="md"
           px="10px"
         >
