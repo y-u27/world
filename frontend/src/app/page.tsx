53a5655732e3087478393238778aeaca8ade5ef2
Atd27 <y518a480@gmail.com>
2026-03-29 23:32:54 +0900

    サイト説明コンポーネントをマップページコンポーネントへ置き換え

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,5 @@
-import { getServerSession } from "next-auth";
 import WorldMapPage from "../components/WorldMapPage";
-import { authOptions } from "./lib/auth";
-import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-  if (!session) return redirect("/login");
-  return <WorldMapPage userId={session.user.id} />;
+export default function Home() {
+  return <WorldMapPage />;
 }
