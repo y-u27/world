3c4b662f1f28111394cfdad461f31df5b1717a12
Atd27 <y518a480@gmail.com>
2026-03-29 22:39:38 +0900

    サイト説明コンポーネント削除→マップページコンポーネントへ変更

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,10 @@
-import IntroHowTo from "../components/IntroHowTo";
+import { getServerSession } from "next-auth";
+import WorldMapPage from "../components/WorldMapPage";
+import { authOptions } from "./lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return <IntroHowTo />;
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session) return redirect("/login");
+  return <WorldMapPage userId={session.user.id} />;
 }
