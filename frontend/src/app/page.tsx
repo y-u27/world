85bfc8de50aebe67d3677c067a327cc3610826a8
Atd27 <y518a480@gmail.com>
2026-03-31 22:59:01 +0900

    コメント削除

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,10 +3,3 @@ import WorldMapPage from "../components/WorldMapPage";
 export default function Home() {
   return <WorldMapPage />;
 }
-
-// ↓一時的に表示
-// import IntroHowTo from "../components/IntroHowTo";
-
-// export default function Home() {
-//   return <IntroHowTo />;
-// }
