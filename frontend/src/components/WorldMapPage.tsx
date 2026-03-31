4ddc0f331b36829b5d5ad3e97948beee3dd91498
Atd27 <y518a480@gmail.com>
2026-03-31 22:59:11 +0900

    修正

--- frontend/src/components/WorldMapPage.tsx
+++ frontend/src/components/WorldMapPage.tsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import PostLists from "./PostLists";
 import SearchBar from "./SearchBar";
 import IntroHowTo from "./IntroHowTo";
+import { useSession } from "next-auth/react";
 
 type Props = {
   userId?: number;
