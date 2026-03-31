75466693f4b5678c46b05084650fbcc1634c2b34
Atd27 <y518a480@gmail.com>
2026-03-31 21:55:17 +0900

    サイト説明を一時的に表示

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,12 @@
-import WorldMapPage from "../components/WorldMapPage";
+// import WorldMapPage from "../components/WorldMapPage";
+
+// export default function Home() {
+//   return <WorldMapPage />;
+// }
+
+// ↓一時的に表示
+import IntroHowTo from "../components/IntroHowTo";
 
 export default function Home() {
-  return <WorldMapPage />;
+  return <IntroHowTo />;
 }
