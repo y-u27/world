a8c007f066abb35839ceb6e9b46ddfb6d3842c84
Atd27 <y518a480@gmail.com>
2026-04-13 16:36:55 +0900

    💄 モーダルウィンドウへUI変更

--- frontend/src/components/Notice.tsx
+++ frontend/src/components/Notice.tsx
@@ -1,6 +1,16 @@
 "use client";
 
-import { Box, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 type Notice = {
@@ -14,7 +24,7 @@ async function fetchAllNotice() {
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/notice`,
     {
       cache: "no-cache",
-    }
+    },
   );
 
   const noticeData = await noticeRes.json();
@@ -23,6 +33,7 @@ async function fetchAllNotice() {
 
 const Notice = () => {
   const [notices, setNotices] = useState<Notice[]>([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     const getNoticeData = async () => {
@@ -47,43 +58,58 @@ const Notice = () => {
           boxShadow="xl"
           borderRadius="10px"
         >
-          <Heading
-            position="absolute"
-            top="-20px"
-            left="20px"
-            bg="white"
-            px={[2, 4, 6]}
-            transform="rotate(-3deg)"
-            bgColor="teal.100"
-            borderRadius={10}
-            bgGradient="linear(to-r, teal.300, blue.200,gray.100)"
-            fontWeight="none"
-            size={["md", "lg", "xl"]}
-          >
-            お知らせ
-          </Heading>
-          {/* お知らせAPIを介してNewsテーブルからデータ取得 */}
-          <Box height="3em" overflow="auto">
-            {notices.length === 0 ? (
-              <Text mt="30px" ml="2%" color="gray.500">
-                ※お知らせはありません
-              </Text>
-            ) : (
-              notices.map((notice) => (
-                <Text display="flex" w="100%" ml="2%" mt="30px" key={notice.id}>
-                  {new Date(notice.createdAt).toLocaleDateString("ja-JP", {
-                    timeZone: "UTC",
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                  {notice.content}
-                </Text>
-              ))
-            )}
-          </Box>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalBody>
+                <ModalHeader
+                  position="absolute"
+                  top="-20px"
+                  left="20px"
+                  bg="white"
+                  px={[2, 4, 6]}
+                  transform="rotate(-3deg)"
+                  bgColor="teal.100"
+                  borderRadius={10}
+                  bgGradient="linear(to-r, teal.300, blue.200,gray.100)"
+                  fontWeight="none"
+                >
+                  お知らせ
+                </ModalHeader>
+                {/* お知らせAPIを介してNewsテーブルからデータ取得 */}
+                <Box height="3em" overflow="auto">
+                  {notices.length === 0 ? (
+                    <Text mt="30px" ml="2%" color="gray.500">
+                      ※お知らせはありません
+                    </Text>
+                  ) : (
+                    notices.map((notice) => (
+                      <Text
+                        display="flex"
+                        w="100%"
+                        ml="2%"
+                        mt="30px"
+                        key={notice.id}
+                      >
+                        {new Date(notice.createdAt).toLocaleDateString(
+                          "ja-JP",
+                          {
+                            timeZone: "UTC",
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          },
+                        )}
+                        {notice.content}
+                      </Text>
+                    ))
+                  )}
+                </Box>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
         </Box>
       </Box>
     </>
