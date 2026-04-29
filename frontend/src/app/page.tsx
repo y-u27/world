f2e16260b2280b652f7d323ad77333a0209949b0
Atd27 <y518a480@gmail.com>
2026-04-29 20:47:15 +0900

    検索用コンポーネント削除

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client"
 
-import { useState } from "react";
-import SearchBar from "../components/SearchBar";
 import WorldMapPage from "../components/WorldMapPage";
 
 export default function Home() {
-  const [searchCountry, setSearchCountry] = useState("");
 
   return (
     <>
-      <SearchBar onSearch={setSearchCountry} />
       <WorldMapPage />
     </>
   );
